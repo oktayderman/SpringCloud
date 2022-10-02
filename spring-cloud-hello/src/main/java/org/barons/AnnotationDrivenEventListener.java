ac85cb2e30822a5f42c79be5b0b448dbe320049b
oktay.cekmez <oktay.cekmez@i2i-systems.com>
2022-10-02 21:11:20 +0300

    event listeners

---
+++ spring-cloud-hello/src/main/java/org/barons/AnnotationDrivenEventListener.java
@@ -0,0 +1,25 @@
+package org.barons;
+
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.ContextStartedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+@EnableAsync
+public class AnnotationDrivenEventListener {
+    @PostConstruct
+    void init() {
+        System.out.println("inited");
+    }
+
+    @Async
+    @EventListener //synch to make it asynch mark as asynch
+    public void handleContextStart(ContextRefreshedEvent cse) {
+        System.out.println("started");
+    }
+}
