89f5198daf52ef7368640eec56a9a2730a27c63d
oktay.cekmez <oktay.cekmez@i2i-systems.com>
2022-10-02 21:27:14 +0300

    event listeners

--- spring-cloud-hello/src/main/java/org/barons/AnnotationDrivenEventListener.java
+++ spring-cloud-hello/src/main/java/org/barons/AnnotationDrivenEventListener.java
@@ -1,11 +1,9 @@
 package org.barons;
 
 import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.context.event.ContextStartedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.annotation.Order;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
--- spring-cloud-hello/src/main/java/org/barons/dto/BookDTO.java
+++ spring-cloud-hello/src/main/java/org/barons/dto/BookDTO.java
@@ -3,17 +3,12 @@ package org.barons.dto;
 import lombok.*;
 
 
+@Getter
+@Setter
 public class BookDTO {
-    private long id;
+    private Long id;
     private String name;
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
 
     public String getName() {
         return name;
